2c9924484bdbf5651377986b0fca74341d06fb2d
Francisco Carranza <Carranza12>
2022-08-25 19:13:12 -0500

    cambios

--- src/app/admin/pages/estadisticas/estadisticas.component.ts
+++ src/app/admin/pages/estadisticas/estadisticas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetCollectionsService } from 'src/app/services/get-collections.service';
 
 @Component({
   selector: 'app-estadisticas',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadisticas.component.scss']
 })
 export class EstadisticasComponent implements OnInit {
-
-  constructor() { }
+public totalPresupuestos:number=0;
+public totalClientes:number=0;
+public totalValue:number=0;
+  constructor(private _presupuestos:GetCollectionsService) { }
 
   ngOnInit(): void {
+    this.loadStatics()
   }
 
+  async loadStatics(){
+    const presupuestos= await this._presupuestos.getPresupuestos();
+    const clientes= await this._presupuestos.getClients();
+    console.log(presupuestos);
+     this.totalPresupuestos=presupuestos.length;
+     this.totalClientes=clientes.length;
+     this.totalValue= presupuestos.map(item=>item.total).reduce((prev,curr)=>prev+curr,0)
+  }
 }
